aebf94ac0b0fe2731796037ad9ea6612e34c2597
Martin Gallegos <magallegosp@gmail.com>
2018-12-01 23:37:30 -0700

    Adding components

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
     constructor(props) {
         super(props);
         console.log('Persons.js Inside Constructor', props);
@@ -13,6 +13,26 @@ class Persons extends Component {
 
     componentDidMount() {
         console.log('Persons.js Inside componentDidMount');
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('UPDATE Person.js Inside componentWillReceiveProps', nextProps);
+    }
+
+    /* shouldComponentUpdate(nextProps, nextState) {
+        console.log('UPDATE Persons.js Inside shouldComponentUpdate', nextProps, nextState);
+        return nextProps.persons !== this.props.persons ||
+            nextProps.changed !== this.props.changed ||
+            nextProps.clicked !== this.props.clicked;
+        //return true;
+    } */
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('Update Persons.js Inside ComponentWillUpdate', nextProps, nextState);
+    }
+
+    componentDidUpdate() {
+        console.log('Update Persons.js Inside ComponentDidUpdate');
     }
 
     render() {
